62e44bb6778679291427c1086e1d10827270afa6
demoemailtmt <demoemailtmt@gmail.com>
2015-07-04 09:10:59 +0700

    test token
    test token

--- module/Instagram/src/Instagram/Controller/InstagramController.php
+++ module/Instagram/src/Instagram/Controller/InstagramController.php
@@ -48,7 +48,7 @@ class InstagramController extends AbstractActionController {
        
         $accessToken= "1953016610.8c7478e.550be686ec1f486099ccab8aba370b16";//$cf["config"]["access_token"];
         $clientKey ="8c7478e0442d4e4ea0ecab768e66b7e2";//$cf["config"]["client_id"];
-         echo $accessToken;echo "hehehe";exit;
+        // echo $accessToken;echo "hehehe";exit;
         if (!$auth) {
             // if the call doesn't requires authentication
             $authMethod = '?client_id=' . $clientKey;
